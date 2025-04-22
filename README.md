f62c33d59ce8d0265d1dcc17e13b0b29de4e837f
DemiusLs <dloss320@gmail.com>
2025-04-22 18:22:07 +0200

    feat:  pari or dispari done

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Dati :
  - [ ] Chiedere all'utente una parola
 
  Elaborazione dati:
- - [ ] Per essere palindroma una parola deve avere la prima lettera uguale all'ultima , la seconda uguale alla penultima 
+ - [ ] Per essere palindroma una parola deve avere la prima lettera uguale all'ultima , la seconda uguale alla penultima faccio un confronto nel ciclo tra questi caratteri
 
 ## Es 2 Pari e dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -17,3 +17,13 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 
+Dati :
+ - [ ] Chiedere all'utente se pari o dispari
+ - [ ] Chiedere all'utente un numero da 1 a 5
+
+
+ Elaborazione dati:
+ - [ ] Generare un numero random da 1 a 5 per il computer
+ - [ ] Sommare i due numeri
+ - [ ] Stabilire se la somma è pari o dispari
+ - [ ] Dichiarare il vincitore
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Esercizio 1 
 const word = "Rossor";
 // prompt("inserisci una parola e ti dirò se palindroma");
 // per il momento commentata per evitare il popup
@@ -8,7 +9,6 @@ console.log(creaArray(word));
 
 
 function creaArray(stringa) {
-    let array = [];
     let result = false;
     let message;
 
@@ -26,18 +26,64 @@ function creaArray(stringa) {
         } else {
             // message = `i caratteri in posizione ${i} e ${lastChar - i} non sono uguali, la parola non è palindroma`;
             result = false;
-            break
+            break;
         }
     }
     if (result == true) {
-        message = "la parola è palindroma"
+        message = "la parola è palindroma";
     } else {
-        message = "la parola non è palindroma"
+        message = "la parola non è palindroma";
     }
     return message;
 
 
 }
+
+// Esercizio 2 
+
+const usernNum = "4";
+const userChoice = "pari"
+// prompt("Inserisci un numero da 1 a 5")
+// prompt("Scegli pari o dispari")
+let userNumInt = parseInt(usernNum);
+
+let computerNumInt = getRandom(1, 5);
+console.log(computerNumInt);
+
+console.log(getWinner(userNumInt, computerNumInt, userChoice));
+
+
+
+function getRandom(min, max) {
+
+    return random = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getWinner(playerNum, pcNum, playerChoice) {
+
+    let summ = playerNum + pcNum;
+    let numb = "";
+    let message;
+
+    if (summ % 2 === 0) {
+
+        numb = "pari";
+    } else {
+        numb = "dispari";
+    }
+
+
+    if (playerChoice == numb) {
+
+        message = `player vince, ${playerNum} + ${pcNum} è ${numb}`;
+    } else {
+        message = `player perde, ${playerNum} + ${pcNum} è ${numb}`;
+    }
+
+    return message;
+
+}
+
 
 
 
